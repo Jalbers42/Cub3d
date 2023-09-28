3d06ffd501eec8da29eb1fafcdce1b49a5d3b06f
yannCardona <ycardona@student.42berlin.de>
2023-09-28 18:25:41 +0200

    walking

--- src/main.c
+++ src/main.c
@@ -6,9 +6,14 @@
 /*   By: ycardona <ycardona@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2023/09/28 11:20:29 by ycardona         ###   ########.fr       */
+/*   Updated: 2023/09/28 18:24:52 by ycardona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#define LEFT 0
+#define RIGHT 1
+#define ROT_SPEED 0.1
+#define MOV_SPEED 0.5
 
 #include "cub3d.h"
 
@@ -19,19 +24,64 @@ static void ft_error(void)
 	exit(EXIT_FAILURE);
 }
 
+void	ft_rotate(int rot_dir, t_game *game)
+{
+	t_vector old_dir;
+	t_vector old_plane;
+
+	old_dir = game->dir;
+	old_plane = game->plane;
+	if (rot_dir == LEFT)
+	{
+		game->dir.x = old_dir.x * cos(-ROT_SPEED) - old_dir.y * sin(-ROT_SPEED);
+		game->dir.y = old_dir.x * sin(-ROT_SPEED) + old_dir.y * cos(-ROT_SPEED);
+	 	game->plane.x = old_plane.x * cos(-ROT_SPEED) - old_plane.y * sin(-ROT_SPEED);
+		game->plane.y = old_plane.x * sin(-ROT_SPEED) + old_plane.y * cos(-ROT_SPEED);
+	}
+	if (rot_dir == RIGHT)
+	{
+		game->dir.x = old_dir.x * cos(ROT_SPEED) - old_dir.y * sin(ROT_SPEED);
+		game->dir.y = old_dir.x * sin(ROT_SPEED) + old_dir.y * cos(ROT_SPEED);
+		game->plane.x = old_plane.x * cos(ROT_SPEED) - old_plane.y * sin(ROT_SPEED);
+		game->plane.y = old_plane.x * sin(ROT_SPEED) + old_plane.y * cos(ROT_SPEED);
+	}
+}
+
+void	ft_move(int mov_dir, t_game *game)
+{
+	if (mov_dir == MLX_KEY_W)
+    {
+    	if(game->map[(int)game->pos.y][(int)(game->pos.x + game->dir.x * MOV_SPEED)] == '0')
+	  		game->pos.x += game->dir.x * MOV_SPEED;
+		if(game->map[(int)(game->pos.y + game->dir.y * MOV_SPEED)][(int)game->pos.x] == '0')
+	  		game->pos.y += game->dir.y * MOV_SPEED;
+	}
+    if (mov_dir == MLX_KEY_S)
+    {
+		if(game->map[(int)game->pos.y][(int)(game->pos.x - game->dir.x * MOV_SPEED)] == '0')
+	  		game->pos.x -= game->dir.x * MOV_SPEED;
+		if(game->map[(int)(game->pos.y - game->dir.y * MOV_SPEED)][(int)game->pos.x] == '0')
+			game->pos.y -= game->dir.y * MOV_SPEED;
+    }
+
+}
 void	ft_key_hook(mlx_key_data_t keydata, void* param)
 {
 	t_game *game = param;
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
 		mlx_close_window(game->mlx);
 	if (mlx_is_key_down(game->mlx, MLX_KEY_W))
-		game->pos.y += 0.1;
+		ft_move(MLX_KEY_W, game);
 	if (mlx_is_key_down(game->mlx, MLX_KEY_S))
-		game->pos.y -= 0.1;
+		ft_move(MLX_KEY_S, game);
 	if (mlx_is_key_down(game->mlx, MLX_KEY_D))
 		game->pos.x -= 0.1;
 	if (mlx_is_key_down(game->mlx, MLX_KEY_A))
 		game->pos.x += 0.1;
+	if (mlx_is_key_down(game->mlx, MLX_KEY_LEFT))
+		ft_rotate(LEFT, game);
+	if (mlx_is_key_down(game->mlx, MLX_KEY_RIGHT))
+		ft_rotate(RIGHT, game);
 	raycasting(game);
 }
 
